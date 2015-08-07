9a8234bf2be499ecdff66b3d04983ec946d31339
Aparajita Fishman <aparajita@aparajita.com>
2015-08-07 17:43:26 +0300

    Documented which Parser methods were overridden

--- index.js
+++ index.js
@@ -4,3 +4,13 @@ exports.cli = require("./lib/cli");
 exports.parse = exports.cli.parse;
 exports.parseFile = exports.cli.parseFile;
 exports.utils = require("./lib/utils");
+
+/*
+    The following acorn.Parser methods were partially or completely overridden
+    and need to be checked in the future for changes to the upstream code:
+
+    parse
+    readWord
+    readWord1
+    skipSpace
+*/
