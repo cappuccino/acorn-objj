001fae4aee47701c229be84500b8c9144c5c04cc
Aparajita Fishman <aparajita@aparajita.com>
2015-08-22 22:32:48 -0400

    Some init had to wait until runtime
    readWord is overridden in tokenize.js and pre-tokenize.js, so the methods overrides have to be done at runtime, not load time.
    
    Also some fixes for code coverage and linting.

--- test/objj/tokenizer.js
+++ test/objj/tokenizer.js
@@ -1,13 +1,25 @@
 "use strict";
 
-var makeParser = require("../lib/test-utils").makeParser;
+var utils = require("../lib/test-utils");
+
+// jscs: disable requireMultipleVarDecl
+
+var makeParser = utils.makeParser,
+    testFixture = utils.testFixture;
 
 /* global describe, it */
 
+// jscs: enable
 // jscs: disable maximumLineLength
 
 describe("Tokenizer", function()
 {
+    // This is for code coverage
+    it("tokenizer should use objj override of readWord with --no-preprocessor", function()
+    {
+        testFixture("objj", "ivars/types", { objjOptions: { preprocessor: false }});
+    });
+
     it("tokenizer should fail with unrecognized @ keywords", function()
     {
         makeParser("@foo").should.throw(SyntaxError, /^Unrecognized Objective-J keyword/);
